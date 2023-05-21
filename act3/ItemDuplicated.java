cdab99624139961986e9d7a326a6491833742548
Percy Carlos Diaz Tupayachi <percy.diaz@ucsm.edu.pe>
2023-05-21 15:33:45 -0500

    Laboratorio 8

---
+++ act3/ItemDuplicated.java
@@ -0,0 +1,10 @@
+package act3;
+
+public class ItemDuplicated extends Exception {
+	public ItemDuplicated(String msg) {
+		super(msg);
+	}
+	public ItemDuplicated() {
+		super();
+	}
+}
---
+++ act8/ItemDuplicated.java
@@ -0,0 +1,10 @@
+package act8;
+
+public class ItemDuplicated extends Exception {
+	public ItemDuplicated(String msg) {
+		super(msg);
+	}
+	public ItemDuplicated() {
+		super();
+	}
+}
